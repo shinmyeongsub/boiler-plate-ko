f01f3884aaf5d3f9de24de4c80b19cb5e4a7c819
shinmyeongsub <joyful2529@naver.com>
2021-10-15 09:57:45 +0900

    confirm user info

--- index.js
+++ index.js
@@ -40,4 +40,31 @@ app.post('/register',(req,res)=>{
     })
 })
 
+app.post('/login',(req,res)=>{
+
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(!user){
+            return res.json({
+                loginSurccess : false,
+                message:"제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+                return res.json({loginSuccess:false, message : "비밀번호가 틀렸습니다." });
+            
+            // 비밀번호 까지 맞다면 토큰 생성
+            user.generateToken((err,user) => {
+                if(err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지 등등
+            
+            })
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
