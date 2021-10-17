0e229c47a2fe9a52f72d2f4a623470be094f527c
shinmyeongsub <joyful2529@naver.com>
2021-10-18 08:36:37 +0900

    2021/10/18 백단 회원가입, 로그인, 로그아웃

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const {User} = require("./models/User");
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const {auth} = require('./middleware/auth');
 
 //application/x-www-form=urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://shin:1234@boilerplate.ofxdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -25,7 +27,8 @@ mongoose.connect('mongodb+srv://shin:1234@boilerplate.ofxdu.mongodb.net/myFirstD
 
 app.get('/', (req, res) => res.send('Hello Wolrd!'))
 
-app.post('/register',(req,res)=>{
+// 회원가입  기능 구현
+app.post('api/users/register',(req,res)=>{
 
     // 회원 가입시 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
@@ -40,13 +43,14 @@ app.post('/register',(req,res)=>{
     })
 })
 
-app.post('/login',(req,res)=>{
+//  로그인 기능 구현
+app.post('/api/users/login',(req,res)=>{
 
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({email:req.body.email},(err,user)=>{
         if(!user){
             return res.json({
-                loginSurccess : false,
+                loginSuccess : false,
                 message:"제공된 이메일에 해당하는 유저가 없습니다."
             })
         }
@@ -61,10 +65,40 @@ app.post('/login',(req,res)=>{
                 if(err) return res.status(400).send(err);
 
                 // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지 등등
-            
+                res.cookie("x_auth", user.token) 
+                .status(200)
+                .json({loginSuccess:true, userId:user._id})
+
             })
         })
     })
+})
+
+// auth - 미들웨어
+app.get('/api/users/auth', auth ,(req,res) =>{
+
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
+    res.status(200).json({
+        _id:req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email : req.user.email,
+        name:req.user.name,
+        lastname:req.user.lastname,
+        role:req.user.role,
+        image:req.user.image
+    })
+})
+
+app.get('/api/users/logout',auth,(req,res) =>{
+    User.findOneAndUpdate({_id:req.user._id},
+        {token : ""},
+        (err,user)=>{
+            if(err)return res.json({success:false, err});
+            return res.status(200).send({
+                success:true
+            })
+        })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
